84d0e6bff10471b24d3346b522e7c67d9ec1cbca
Maksim Vishnevskiy <maksvish135@yandex.ru>
2023-11-05 22:35:05 +0300

    Change Constraint layout to Coordinator layout

--- app/src/main/java/ru/samsung/academy/m20i/MainActivity.kt
+++ app/src/main/java/ru/samsung/academy/m20i/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
 
 
     }
